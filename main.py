defe88d63e9d80437d72d51e774a733d4b097002
Matt Hardwick <matthardwick@gmail.com>
2016-11-09 01:49:04 -0800

    course update
    templates

--- main.py
+++ main.py
@@ -1,115 +1,66 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
 import webapp2
+import jinja2
 
-form = """
-<form method="post">
-    what is dob?
-    <br>
-    <label>month <input type="text" name="month" value="%(month)s"></label><br>
-    <label>day <input type="text" name="day" value="%(day)s"></label><br>
-    <label>year <input type="text" name="year" value="%(year)s"></label>
-    <div style="color: red">%(error)s</div>
-    <br>
-    <input type="submit">
-</form>
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
+
+
+
+hidden_html = """
+<input type="hidden" name="food" value="%s">
 """
 
-months = ['January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July',
-          'August',
-          'September',
-          'October',
-          'November',
-          'December']
+item_html = "<li>%s</li>"
+
+shopping_list_html = """
+<br>
+<br>
+<h2>Shopping List</h2>
+<ul>
+%s
+</ul>
+"""
 
 
-def valid_month(month):
-    if month.lower().capitalize() in months:
-        return (month.capitalize())
-    else:
-        return None
+class Handler(webapp2.RequestHandler):
+
+    def write(self, *a, **kw):
+        self.response.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
 
 
-def valid_day(day):
-    if day and day.isdigit():
-        if day >= '1' and day <= '31':
-            dayi = int(float(day))
-            return (dayi)
-
-
-def valid_year(year):
-    if year and year.isdigit():
-        year = int(year)
-        if year >= 1900 and year <= 2020:
-            return year
-    else:
-        return None
-
-
-def escape_html(s):
-    for (i, o) in (("&", "&amp;"),
-                   (">", "&gt;"),
-                   ("<", "&lt;"),
-                   ('"', "&quot;")):
-        s = s.replace(i, o)
-    return s
-
-
-class MainPage(webapp2.RequestHandler):
-
-    def write_form(self, error="", month="", day="", year=""):
-        self.response.out.write(form % {"error": error,
-                                        "month": escape_html(month),
-                                        "day": escape_html(day),
-                                        "year": escape_html(year)})
+class MainPage(Handler):
 
     def get(self):
-        # self.response.headers['Content-Type'] = 'text/plain'
-        # self.response.out.write(form)
-        self.write_form()
+        n = self.request.get("n")
+        if n:
+            n = int(n)
+        self.render("shopping_list.html", n=n)
+        # output = form_html
+        # output_hidden = ""
 
-    def post(self):
-        user_month = self.request.get('month')
-        user_day = self.request.get('day')
-        user_year = self.request.get('year')
+        # items = self.request.get_all("food")
+        # if items:
+        #     output_items = ""
 
-        month = valid_month(user_month)
-        day = valid_day(user_day)
-        year = valid_year(user_year)
+        #     for item in items:
+        #         output_hidden += hidden_html % item
+        #         output_items += item_html % item
 
-        if not (month and day and year):
-            self.write_form("errrorrrrr!!!",
-                            user_month, user_day, user_year)
-        else:
-            self.redirect("/thanks")
+        #     output_shopping = shopping_list_html % output_items
+        #     output += output_shopping
 
+        # output = output % output_hidden
 
-class ThanksHandler(webapp2.RequestHandler):
-
-    def get(self):
-        self.response.out.write("thanks!")
-
+        # self.write(output)
 
 app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/thanks', ThanksHandler)],
+                               ],
                               debug=True)
